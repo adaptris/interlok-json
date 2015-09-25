7b634fa115892917384e975cf360110d4dc1d26a
Aaron McGrath <aaron.mcgrath@adaptris.com>
2015-09-25 14:42:49 +0100

    INTERLOK-644 #time 2d Initial checkin of the json transformer with Jolt. Needs some cleanup.

---
+++ test/src/com/adaptris/core/transform/json/JsonTransformServiceTest.java
@@ -0,0 +1,161 @@
+package com.adaptris.core.transform.json;
+
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.DefaultMessageFactory;
+import com.adaptris.core.transform.TransformServiceExample;
+
+public class JsonTransformServiceTest extends TransformServiceExample {
+  
+  private static final String METADATA_KEY = "data-key";
+  
+  private JsonTransformService service;
+  private PayloadDataDestination payloadDataDestination;
+  private MetadataDataDestination metadataDataDestination;
+  
+  private AdaptrisMessage message;
+
+  public JsonTransformServiceTest(String name) {
+    super(name);
+  }
+
+  public void setUp() throws Exception {
+    service = new JsonTransformService();
+    payloadDataDestination = new PayloadDataDestination();
+    metadataDataDestination = new MetadataDataDestination();
+    metadataDataDestination.setMetadataKey(METADATA_KEY);
+    
+    message = DefaultMessageFactory.getDefaultInstance().newMessage();
+  }
+  
+  public void tearDown() throws Exception {
+    
+  }
+  
+  public void testSimpleTransform() throws Exception {
+    service.setSourceJsonDestination(payloadDataDestination);
+    service.setSourceSpecDestination(metadataDataDestination);
+    service.setTargetResultDestination(payloadDataDestination);
+    
+    message.setStringPayload(sampleInput, message.getCharEncoding());
+    message.addMetadata(METADATA_KEY, sampleSpec);
+    
+    service.doService(message);
+    
+    assertEquals(sampleOutput, message.getStringPayload());
+  }
+  
+  public void testSimpleTransformWithVarSubButNoMetadatMatches() throws Exception {
+    service.setSourceJsonDestination(payloadDataDestination);
+    service.setSourceSpecDestination(metadataDataDestination);
+    service.setTargetResultDestination(payloadDataDestination);
+    
+    service.setApplyMetadataAsParameters(true);
+    
+    message.setStringPayload(sampleInput, message.getCharEncoding());
+    message.addMetadata(METADATA_KEY, sampleSpec);
+    message.addMetadata("SomeKey", "SomeValue");
+    
+    service.doService(message);
+    
+    assertEquals(sampleOutput, message.getStringPayload());
+  }
+  
+  public void testSimpleTransformWithVarSub() throws Exception {
+    service.setSourceJsonDestination(payloadDataDestination);
+    service.setSourceSpecDestination(metadataDataDestination);
+    service.setTargetResultDestination(payloadDataDestination);
+    
+    service.setApplyMetadataAsParameters(true);
+    
+    message.setStringPayload(sampleInput, message.getCharEncoding());
+    message.addMetadata(METADATA_KEY, sampleSpecVarSub);
+    message.addMetadata("tertiary-ratings", "TertiaryRatings");
+    
+    service.doService(message);
+    
+    assertEquals(sampleOutputVarSub, message.getStringPayload());
+  }
+  
+  @Override
+  protected Object retrieveObjectForSampleConfig() {
+    service.setSourceJsonDestination(payloadDataDestination);
+    service.setSourceSpecDestination(metadataDataDestination);
+    service.setTargetResultDestination(new PayloadDataDestination());
+    
+    return service;
+  }
+
+  private String sampleInput = ""
+      + "{" + 
+    "\"rating\": {" + 
+    "    \"primary\": {" + 
+    "        \"values\": 3" +
+    "    }," +
+    "    \"quality\": {" +
+    "        \"values\": 3" +
+    "    }" +
+    "}" +
+    "}";
+  
+  private String sampleSpec = ""
+      + "[" +
+    "{" + 
+    "    \"operation\": \"shift\"," + 
+    "    \"spec\": {" + 
+    "        \"rating\": {" + 
+    "            \"primary\": {" + 
+    "                \"values\": \"Rating\"" +
+    "            }," +
+    "            \"*\": {" +
+    "                \"values\": \"SecondaryRatings.&1.Value\"," +
+    "                \"$\": \"SecondaryRatings.&.Id\"" + 
+    "            }" +
+    "        }" +
+    "    }" +
+    "}," +
+    "{" +
+    "    \"operation\": \"default\"," + 
+    "    \"spec\": {" +
+    "        \"Range\" : 5," +
+    "        \"SecondaryRatings\" : {" +
+    "            \"*\" : {" +
+    "                \"Range\" : 5" +
+    "            }" +
+    "        }" +
+    "    }" +
+    "}" +
+    "]";
+  
+  private String sampleSpecVarSub = ""
+      + "[" +
+    "{" + 
+    "    \"operation\": \"shift\"," + 
+    "    \"spec\": {" + 
+    "        \"rating\": {" + 
+    "            \"primary\": {" + 
+    "                \"values\": \"Rating\"" +
+    "            }," +
+    "            \"*\": {" +
+    "                \"values\": \"${tertiary-ratings}.&1.Value\"," +
+    "                \"$\": \"${tertiary-ratings}.&.Id\"" + 
+    "            }" +
+    "        }" +
+    "    }" +
+    "}," +
+    "{" +
+    "    \"operation\": \"default\"," + 
+    "    \"spec\": {" +
+    "        \"Range\" : 5," +
+    "        \"${tertiary-ratings}\" : {" +
+    "            \"*\" : {" +
+    "                \"Range\" : 5" +
+    "            }" +
+    "        }" +
+    "    }" +
+    "}" +
+    "]";
+  
+  private String sampleOutput = "{\"Rating\":3,\"SecondaryRatings\":{\"quality\":{\"Id\":\"quality\",\"Value\":3,\"Range\":5}},\"Range\":5}";
+  
+  private String sampleOutputVarSub = "{\"Rating\":3,\"TertiaryRatings\":{\"quality\":{\"Id\":\"quality\",\"Value\":3,\"Range\":5}},\"Range\":5}";
+}
