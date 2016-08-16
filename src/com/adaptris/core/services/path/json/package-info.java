692cc35f62289af5e298fc8f08abcbb5960ed30f
Lewin Chan <lewin.chan@adaptris.com>
2016-08-16 14:51:14 +0100

    Added dependency on umldoclet which allows us to create UML diagrams based on the code (does increase the time taken to do the javadocs).

---
+++ src/com/adaptris/core/services/path/json/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * <p>
+ * Implementation(s) of {@link com.adaptris.core.Service} providing the ability
+ * to extract data from a JSON object.
+ * </p>
+ * 
+ * <img alt="UML" src="package.svg"/>
+ * 
+ */
+package com.adaptris.core.services.path.json;
--- src/com/adaptris/core/transform/json/package-info.java
+++ src/com/adaptris/core/transform/json/package-info.java
@@ -1,4 +1,7 @@
 /**
+ * <p>
  * Implementation of the core {@link com.adaptris.core.Service} interface providing JSON transformation functionality.
+ * </p>
+ * <img alt="UML" src="package.svg"/>
  */
 package com.adaptris.core.transform.json;
