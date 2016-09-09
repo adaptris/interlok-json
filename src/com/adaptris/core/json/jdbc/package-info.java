6f49ae66886cc254e425412b2c33e9eff63a8c59
Ashley Anderson <ashley.anderson@reedbusiness.com>
2016-09-09 09:09:13 +0100

    Add package-info.java file

---
+++ src/com/adaptris/core/json/jdbc/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Implementation(s) of {@link com.adaptris.core.services.jdbc} providing the ability
+ * to extract data from a JDBC result set.
+ *
+ * <img alt="UML" src="package.svg"/>
+ *
+ */
+package com.adaptris.core.json.jdbc;
