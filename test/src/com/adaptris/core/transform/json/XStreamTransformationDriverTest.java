d1e3fdd32b130bbbc7a0020099e926545f77faa7
Ashley Anderson <ashley.anderson@reedbusiness.com>
2016-10-07 14:31:25 +0100

    Update unit tests.

--- test/src/com/adaptris/core/transform/json/XStreamTransformationDriverTest.java
+++ test/src/com/adaptris/core/transform/json/XStreamTransformationDriverTest.java
@@ -13,71 +13,69 @@ import com.adaptris.util.GuidGenerator;
 
 public class XStreamTransformationDriverTest extends BaseCase {
 
-  private transient XStreamMarshaller xmlMarshaller;
-  private transient XStreamJsonMarshaller jsonMarshaller;
+	private transient XStreamMarshaller xmlMarshaller;
+	private transient XStreamJsonMarshaller jsonMarshaller;
 
-  public XStreamTransformationDriverTest(String name) {
-    super(name);
-  }
+	public XStreamTransformationDriverTest(final String name) {
+		super(name);
+	}
 
-  public void testToXML() throws Exception {
-    XStreamTransformationDriver driver = new XStreamTransformationDriver();
-    SerializableMessage original = configure(new SerializableAdaptrisMessage());
-    String jsonData = jsonMarshaller().marshal(original);
-    System.err.println(jsonData);
-    String xmlData = driver.transform(jsonData, JsonXmlTransformService.TransformationDirection.JSON_TO_XML);
-    System.err.println(xmlData);
-    SerializableAdaptrisMessage unmarshalled = (SerializableAdaptrisMessage) xmlMarshaller().unmarshal(xmlData);
-    assertEquals(original, unmarshalled);
-  }
+	public void testToXML() throws Exception {
+		final XStreamTransformationDriver driver = new XStreamTransformationDriver();
+		final SerializableMessage original = configure(new SerializableAdaptrisMessage());
+		final String jsonData = jsonMarshaller().marshal(original);
+		System.err.println(jsonData);
+		final String xmlData = driver.transform(jsonData, TransformationDirection.JSON_TO_XML);
+		System.err.println(xmlData);
+		final SerializableAdaptrisMessage unmarshalled = (SerializableAdaptrisMessage)xmlMarshaller().unmarshal(xmlData);
+		assertEquals(original, unmarshalled);
+	}
 
-  public void testToJSON() throws Exception {
-    XStreamTransformationDriver driver = new XStreamTransformationDriver();
-    SerializableMessage original = configure(new SerializableAdaptrisMessage());
-    String xmlData = xmlMarshaller().marshal(original);
-    System.err.println(xmlData);
-    String jsonData = driver.transform(xmlData, JsonXmlTransformService.TransformationDirection.XML_TO_JSON);
-    System.err.println(jsonData);
-    SerializableAdaptrisMessage unmarshalled = (SerializableAdaptrisMessage) jsonMarshaller().unmarshal(jsonData);
-    assertEquals(original, unmarshalled);
-  }
+	public void testToJSON() throws Exception {
+		final XStreamTransformationDriver driver = new XStreamTransformationDriver();
+		final SerializableMessage original = configure(new SerializableAdaptrisMessage());
+		final String xmlData = xmlMarshaller().marshal(original);
+		System.err.println(xmlData);
+		final String jsonData = driver.transform(xmlData, TransformationDirection.XML_TO_JSON);
+		System.err.println(jsonData);
+		final SerializableAdaptrisMessage unmarshalled = (SerializableAdaptrisMessage)jsonMarshaller().unmarshal(jsonData);
+		assertEquals(original, unmarshalled);
+	}
 
+	public void testBadData() throws Exception {
+		final XStreamTransformationDriver driver = new XStreamTransformationDriver();
+		final SerializableMessage original = configure(new SerializableAdaptrisMessage());
+		final String xmlData = xmlMarshaller().marshal(original);
+		try {
+			final String jsonData = driver.transform(xmlData, TransformationDirection.JSON_TO_XML);
+			fail();
+		} catch (final ServiceException expected) {
 
-  public void testBadData() throws Exception {
-    XStreamTransformationDriver driver = new XStreamTransformationDriver();
-    SerializableMessage original = configure(new SerializableAdaptrisMessage());
-    String xmlData = xmlMarshaller().marshal(original);
-    try {
-      String jsonData = driver.transform(xmlData, JsonXmlTransformService.TransformationDirection.JSON_TO_XML);
-      fail();
-    } catch (ServiceException expected) {
+		}
+	}
 
-    }
-  }
+	private SerializableMessage configure(final SerializableMessage msg) {
+		msg.setContent("Pack my bag with a dozen liqour jugs");
+		msg.setUniqueId(new GuidGenerator().getUUID());
+		final int max = ThreadLocalRandom.current().nextInt(10);
+		for (int i = 0; i < max; i++) {
+			msg.addMessageHeader("header_" + i, "value_" + i);
+		}
+		return msg;
+	}
 
+	private XStreamMarshaller xmlMarshaller() throws CoreException {
+		if (xmlMarshaller == null) {
+			xmlMarshaller = new XStreamMarshaller();
+		}
+		return xmlMarshaller;
+	}
 
-  private SerializableMessage configure(SerializableMessage msg) {
-    msg.setContent("Pack my bag with a dozen liqour jugs");
-    msg.setUniqueId(new GuidGenerator().getUUID());
-    int max = ThreadLocalRandom.current().nextInt(10);
-    for (int i = 0; i < max; i++) {
-      msg.addMessageHeader("header_" + i, "value_" + i);
-    }
-    return msg;
-  }
-
-  private XStreamMarshaller xmlMarshaller() throws CoreException {
-    if (xmlMarshaller == null) {
-      xmlMarshaller = new XStreamMarshaller();
-    }
-    return xmlMarshaller;
-  }
-
-  private XStreamJsonMarshaller jsonMarshaller() throws CoreException {
-    if (jsonMarshaller == null) {
-      jsonMarshaller = new XStreamJsonMarshaller();
-    }
-    return jsonMarshaller;
-  }
+	private XStreamJsonMarshaller jsonMarshaller() throws CoreException {
+		if (jsonMarshaller == null) {
+			jsonMarshaller = new XStreamJsonMarshaller();
+		}
+		return jsonMarshaller;
+	}
 
 }
