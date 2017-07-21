2a49dd5bdfe94a4feffa810f8c58ac8edb58e6f8
Lewin Chan <lewin.chan@adaptris.com>
2017-07-21 13:36:17 +0100

    INTERLOK-1496 Added a ExceptionAsJson impl of ExceptionSerializer INTERLOK-1496 #time 2h
    - ExceptionAsJson does not emit the stack trace
    - ExceptionWithStacktrace does emit the stack trace.
    - emits the 'workflowId' where the msg was being processed
    - emits the 'cause' - the service that failed.
    - emits e.getMessage()

---
+++ src/com/adaptris/core/json/exception/ExceptionAsJson.java
@@ -0,0 +1,69 @@
+package com.adaptris.core.json.exception;
+
+import static org.apache.commons.lang.StringUtils.isEmpty;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.apache.commons.io.IOUtils;
+
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.CoreConstants;
+import com.adaptris.core.CoreException;
+import com.adaptris.core.Workflow;
+import com.adaptris.core.services.exception.ExceptionReportService;
+import com.adaptris.core.services.exception.ExceptionSerializer;
+import com.adaptris.core.util.ExceptionHelper;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+/**
+ * Write the exception as a JSON object using {@code jackson-databind} when using a {@link ExceptionReportService}.
+ * <p>
+ * Writes the minimal amount of information possible which is the {@link Exception#getMessage()}; the workflow from where the
+ * message was being processed (if available); and the location of the error (i.e. the service that threw the exception).
+ * </p>
+ * 
+ * @config exception-as-json
+ */
+@XStreamAlias("exception-as-json")
+public class ExceptionAsJson implements ExceptionSerializer {
+
+  private transient ObjectMapper mapper = new ObjectMapper();
+
+  public ExceptionAsJson() {
+  }
+
+  @Override
+  public void serialize(Exception exception, AdaptrisMessage msg) throws CoreException {
+    Writer out = null;
+    try {
+      out = msg.getWriter(encoding(msg));
+      mapper.writerWithDefaultPrettyPrinter().writeValue(out, createReport(exception, msg));
+    }
+    catch (IOException e) {
+      throw ExceptionHelper.wrapCoreException(e);
+    }
+    finally {
+      IOUtils.closeQuietly(out);
+    }
+  }
+
+  private String encoding(AdaptrisMessage msg) {
+    String encoding = "UTF-8";
+    if (!isEmpty(msg.getContentEncoding())) {
+      encoding = msg.getContentEncoding();
+    }
+    return encoding;
+  }
+
+  protected Map<String, Object> createReport(Exception exc, AdaptrisMessage msg) {
+    Map<String, Object> result = new TreeMap<String, Object>();
+    result.put("workflow", msg.getMetadataValue(Workflow.WORKFLOW_ID_KEY));
+    result.put("exceptionLocation", msg.getObjectHeaders().get(CoreConstants.OBJ_METADATA_EXCEPTION_CAUSE));
+    result.put("exceptionMessage", exc.getMessage());
+    return result;
+  }
+}
---
+++ src/com/adaptris/core/json/exception/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <p>
+ * Implementation(s) of {@com.adaptris.core.services.exception.ExceptionSerializer} providing the ability write an exception as
+ * JSON.
+ * </p>
+ * 
+ */
+package com.adaptris.core.json.exception;
