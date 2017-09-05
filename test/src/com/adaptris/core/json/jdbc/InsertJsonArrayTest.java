a5b461dede9dbb0ac97b7c87b2d6bb4b6e56f035
Lewin Chan <lewin.chan@adaptris.com>
2017-09-05 10:20:29 +0100

    INTERLOK-1791 package move.

--- test/src/com/adaptris/core/json/jdbc/InsertJsonArrayTest.java
+++ test/src/com/adaptris/core/json/jdbc/InsertJsonArrayTest.java
@@ -1,11 +1,9 @@
-package com.adaptris.core.services.jdbc;
+package com.adaptris.core.json.jdbc;
 
 import com.adaptris.core.AdaptrisMessage;
 import com.adaptris.core.AdaptrisMessageFactory;
 import com.adaptris.core.ServiceException;
-import com.adaptris.core.jdbc.JdbcConnection;
 import com.adaptris.core.json.jdbc.InsertJsonArray;
-import com.adaptris.util.TimeInterval;
 
 public class InsertJsonArrayTest extends JdbcJsonInsertCase {
 
@@ -14,30 +12,17 @@ public class InsertJsonArrayTest extends JdbcJsonInsertCase {
     super(arg0);
   }
 
-  @Override
-  protected Object retrieveObjectForSampleConfig() {
-    InsertJsonArray service = createService();
-    service.setTable("myTable");
-    JdbcConnection connection = new JdbcConnection();
-    connection.setConnectUrl("jdbc:mysql://localhost:3306/mydatabase");
-    connection.setConnectionAttempts(2);
-    connection.setConnectionRetryInterval(new TimeInterval(3L, "SECONDS"));
-    service.setConnection(connection);
-    return service;
-  }
-
-
   public void testService() throws Exception {
     createDatabase();
-    InsertJsonArray service = configure(createService());
-    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(CONTENT);
+    InsertJsonArray service = configureForTests(createService());
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(ARRAY_CONTENT);
     execute(service, msg);
     doAssert(3);
   }
 
   public void testService_NotArray() throws Exception {
     createDatabase();
-    InsertJsonArray service = configure(createService());
+    InsertJsonArray service = configureForTests(createService());
     AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(OBJECT_CONTENT);
     try {
       execute(service, msg);
@@ -49,7 +34,7 @@ public class InsertJsonArrayTest extends JdbcJsonInsertCase {
 
   public void testService_BrokenColumn() throws Exception {
     createDatabase();
-    InsertJsonArray service = configure(createService());
+    InsertJsonArray service = configureForTests(createService());
     AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(INVALID_COLUMN_ARRAY);
     try {
       execute(service, msg);
