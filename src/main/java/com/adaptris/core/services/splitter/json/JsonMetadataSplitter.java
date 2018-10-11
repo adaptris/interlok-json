d72c96d2f2782ea38e38ca731cc501d75a9e4bcb
Ashley Anderson <ashley.anderson@reedbusiness.com>
2018-10-11 14:04:07 +0100

    Rewrite JSON metadata splitter based on updated specs

--- src/main/java/com/adaptris/core/services/splitter/json/JsonMetadataSplitter.java
+++ src/main/java/com/adaptris/core/services/splitter/json/JsonMetadataSplitter.java
@@ -2,28 +2,18 @@ package com.adaptris.core.services.splitter.json;
 
 import com.adaptris.core.AdaptrisMessage;
 import com.adaptris.core.CoreException;
-import com.adaptris.core.MetadataElement;
-import com.adaptris.core.services.splitter.MessageSplitterImp;
-import com.adaptris.core.util.ExceptionHelper;
+import com.adaptris.core.util.CloseableIterable;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
-import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
-import org.apache.commons.io.IOUtils;
+import net.minidev.json.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
-import java.io.Writer;
-import java.util.ArrayList;
-import java.util.List;
-
 @XStreamAlias("json-metadata-splitter")
-public class JsonMetadataSplitter extends MessageSplitterImp
+public class JsonMetadataSplitter extends LargeJsonArraySplitter
 {
-	private static final Logger LOGGER = LoggerFactory.getLogger(JsonMetadataSplitter.class.getName());
+	private static final Logger logger = LoggerFactory.getLogger(JsonMetadataSplitter.class);
 
 	/**
 	 * Split a JSON payload from an Adaptris message metadata. {@inheritDoc}.
@@ -34,131 +24,30 @@ public class JsonMetadataSplitter extends MessageSplitterImp
 	 * @return A list of Adaptris messages for each JSON object.
 	 */
 	@Override
-	public List<AdaptrisMessage> splitMessage(final AdaptrisMessage message) throws CoreException
+	public CloseableIterable<AdaptrisMessage> splitMessage(final AdaptrisMessage message) throws CoreException
 	{
-		final List<AdaptrisMessage> result = new ArrayList<>();
-		final JSONParser jsonParser = new JSONParser(JSONParser.MODE_PERMISSIVE);
-
-		try
+		final CloseableIterable<AdaptrisMessage> splitMessages = super.splitMessage(message);
+		for (final AdaptrisMessage splitMessage : splitMessages)
 		{
-			for (final MetadataElement me : message.getMetadata())
+			try
 			{
-				final Object object = jsonParser.parse(message.getMetadataValue(me.getKey()));
-				if (object instanceof JSONArray)
+				final String payload = splitMessage.getContent();
+
+				final JSONParser jsonParser = new JSONParser(JSONParser.MODE_PERMISSIVE);
+				final JSONObject json = (JSONObject)jsonParser.parse(payload);
+				for (final String key : json.keySet())
 				{
-					final JSONArray array = (JSONArray)object;
-					if (array.isEmpty())
-					{
-						result.add(message);
-					}
-					else
-					{
-						result.addAll(splitMessage(array, message));
-					}
+					final String value = json.getAsString(key);
+					splitMessage.addMetadata(key, value);
 				}
-				else if (object instanceof JSONObject)
-				{
-					final JSONObject json = (JSONObject)object;
-					if (json.isEmpty())
-					{
-						result.add(message);
-					}
-					else
-					{
-						for (final String key : json.keySet())
-						{
-							final JSONObject o = new JSONObject();
-							o.put(key, json.get(key));
-							result.add(createSplitMessage(o, message));
-						}
-					}
-				}
-				else
-				{
-					throw new CoreException("Message payload was not JSON; could not be parsed to " + JSONObject.class + " from " + object.getClass());
-				}
+
+				splitMessage.setPayload(null);
+			}
+			catch (final ParseException e)
+			{
+				logger.error("Error parsing JSON", e);
 			}
 		}
-		catch (final Exception e)
-		{
-			LOGGER.error("Could not parse or split JSON object payload.", e);
-			throw ExceptionHelper.wrapCoreException(e);
-		}
-		return result;
-	}
-
-	/**
-	 * Split a JSON array into a list of Adaptris messages for each JSON array element.
-	 *
-	 * @param array
-	 * 		The JSON array.
-	 * @param message
-	 * 		The original Adaptris message.
-	 *
-	 * @return A list of Adaptris messages.
-	 *
-	 * @throws IOException
-	 * 		If IOUtils cannot copy from a Reader to a Writer.
-	 */
-	private List<AdaptrisMessage> splitMessage(final JSONArray array, final AdaptrisMessage message) throws IOException
-	{
-		final List<AdaptrisMessage> result = new ArrayList<>();
-		for (final Object element : array)
-		{
-			final AdaptrisMessage splitMessage;
-			if (element instanceof JSONObject)
-			{
-				splitMessage = createSplitMessage((JSONObject)element, message);
-			}
-			else
-			{
-				splitMessage = createSplitMessage(element.toString(), message);
-			}
-			result.add(splitMessage);
-		}
-		return result;
-	}
-
-	/**
-	 * Create a new Adaptris message for the given JSON object.
-	 *
-	 * @param json
-	 * 		The JSON object.
-	 * @param message
-	 * 		The original Adaptris message.
-	 *
-	 * @return A new Adaptris message for the JSON object.
-	 *
-	 * @throws IOException
-	 * 		If IOUtils cannot copy from a Reader to a Writer.
-	 */
-	private AdaptrisMessage createSplitMessage(final JSONObject json, final AdaptrisMessage message) throws IOException
-	{
-		return createSplitMessage(json.toJSONString(), message);
-	}
-
-	/**
-	 * Create a new Adaptris message for the given JSON object.
-	 *
-	 * @param json
-	 * 		The JSON string.
-	 * @param message
-	 * 		The original Adaptris message.
-	 *
-	 * @return A new Adaptris message for the JSON object.
-	 *
-	 * @throws IOException
-	 * 		If IOUtils cannot copy from a Reader to a Writer.
-	 */
-	private AdaptrisMessage createSplitMessage(final String json, final AdaptrisMessage message) throws IOException
-	{
-		final AdaptrisMessage newMessage = selectFactory(message).newMessage();
-		newMessage.addMetadata("json", json);
-		try (final Reader reader = new StringReader(json);
-		     final Writer writer = newMessage.getWriter())
-		{
-			IOUtils.copy(reader, writer);
-		}
-		return newMessage;
+		return splitMessages;
 	}
 }
--- src/test/java/com/adaptris/core/services/splitter/json/JsonMetadataSplitterTest.java
+++ src/test/java/com/adaptris/core/services/splitter/json/JsonMetadataSplitterTest.java
@@ -1,14 +1,13 @@
 package com.adaptris.core.services.splitter.json;
 
 import java.util.List;
-import java.util.Set;
 
 import com.adaptris.core.AdaptrisMessage;
 import com.adaptris.core.AdaptrisMessageFactory;
 import com.adaptris.core.CoreException;
-import com.adaptris.core.MetadataElement;
 import com.adaptris.core.transform.json.JsonToXmlTransformServiceTest;
 
+import com.adaptris.core.util.CloseableIterable;
 import net.sf.json.JSONObject;
 import net.sf.json.JSONSerializer;
 
@@ -19,20 +18,38 @@ public class JsonMetadataSplitterTest extends SplitterServiceExample
 		super(name);
 	}
 
-	public static final String JSON_ARRAY = "[\n{\"colour\": \"red\",\"value\": \"#f00\"},\n"
-			+ "{\"colour\": \"green\",\"value\": \"#0f0\"},\n"
-			+ "{\"colour\": \"blue\",\"value\": \"#00f\"},"
-			+ "\n{\"colour\": \"black\",\"value\": \"#000\"}\n"
+	public static final String PAYLOAD = "[\n"
+			+ "{\"colour\": \"red\"},\n"
+			+ "{\"colour\": \"green\"},\n"
+			+ "{\"colour\": \"blue\"},\n"
+			+ "{\"colour\": \"black\"}\n"
 			+ "]";
+
+	private static final String METADATA = "a=b,b=c";
 
 	public void testSplitArray() throws Exception
 	{
-		final AdaptrisMessage message = AdaptrisMessageFactory.getDefaultInstance().newMessage();
-		message.addMetadata("json", JSON_ARRAY);
+		final AdaptrisMessage message = AdaptrisMessageFactory.getDefaultInstance().newMessage(PAYLOAD);
 
-		assertEquals(4, new JsonMetadataSplitter().splitMessage(message).size());
+		try (final CloseableIterable<AdaptrisMessage> i = createSplitter().splitMessage(message))
+		{
+			for (final AdaptrisMessage m : i)
+			{
+				assertNull(m.getPayload());
+				switch (m.getMetadataValue("colour"))
+				{
+					case "red":
+					case "green":
+					case "blue":
+					case "black":
+						break;
+					default:
+						fail();
+				}
+			}
+		}
 	}
-
+/*
 	public void testSplitObject() throws Exception
 	{
 		final AdaptrisMessage message = AdaptrisMessageFactory.getDefaultInstance().newMessage();
@@ -76,9 +93,9 @@ public class JsonMetadataSplitterTest extends SplitterServiceExample
 		}
 		catch (CoreException expected)
 		{
-			/* expected behaviour */
+			// expected behaviour
 		}
-	}
+	}*/
 
 	@Override
 	protected String getExampleCommentHeader(Object o)
@@ -86,7 +103,7 @@ public class JsonMetadataSplitterTest extends SplitterServiceExample
 		return super.getExampleCommentHeader(o) + "\n<!-- \n If the incoming document is \n\n"
 				+ JsonToXmlTransformServiceTest.JSON_INPUT
 				+ "\n\nthis would create 3 new messages, 1 each for 'entry', 'notes', 'version'." + "\nIf the incoming document is \n\n"
-				+ JSON_ARRAY + "\n\nthis would create 4 messages, 1 for each element" + "\n-->\n";
+				+ PAYLOAD + "\n\nthis would create 4 messages, 1 for each element" + "\n-->\n";
 	}
 
 	@Override
