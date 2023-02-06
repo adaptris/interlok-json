c4e3a59b52105a273ac05445128e91dedb1dfdda
Aaron McGrath <aaron.mcgrath@adaptris.com>
2023-02-06 12:06:04 +0000

    Added ability to specify the signing algorithm.

--- interlok-json-web-token/src/main/java/com/adaptris/core/jwt/secrets/Base64EncodedSecret.java
+++ interlok-json-web-token/src/main/java/com/adaptris/core/jwt/secrets/Base64EncodedSecret.java
@@ -1,14 +1,19 @@
 package com.adaptris.core.jwt.secrets;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 import com.thoughtworks.xstream.annotations.XStreamAlias;
+
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.JwtParserBuilder;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.validation.constraints.NotBlank;
 
 @XStreamAlias("base64-encoded-secret")
 public class Base64EncodedSecret implements SecretConfigurator
@@ -17,11 +22,21 @@ public class Base64EncodedSecret implements SecretConfigurator
   @Setter
   @NotBlank
   private String secret;
-
+  
+  @Getter
+  @Setter
+  @NotBlank
+  @Pattern(regexp = "NONE|HS256|HS384|HS512|RS256|RS384|RS512|ES256|ES384|ES512|PS256|PS384|PS512")
+  private String signingAlgorithm;
+  
   @Override
   public JwtBuilder configure(JwtBuilder builder)
   {
-    return builder.signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret)));
+//    SecretKey hmacShaKeyFor = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+    SignatureAlgorithm algorithmToUse = SignatureAlgorithm.valueOf(getSigningAlgorithm());
+    SecretKey hmacShaKeyFor = new SecretKeySpec(Decoders.BASE64.decode(secret), algorithmToUse.getJcaName());
+    
+    return builder.signWith(hmacShaKeyFor);
   }
 
   @Override
