6c6aa599bf5f53c0d2444a570c972bea4e111ccd
James Wickham <james.wickham@proagrica.com>
2023-02-06 15:11:27 +0000

    adding enum list of allowed signing algorithms which is then called in the Base64EncodedSecret class.

---
+++ interlok-json-web-token/src/main/java/com/adaptris/core/jwt/secrets/JwtSigningAlgorithm.java
@@ -0,0 +1,20 @@
+package com.adaptris.core.jwt.secrets;
+
+public enum JwtSigningAlgorithm {
+	
+	AUTO,
+	NONE,
+	HS256,
+	HS384,
+	HS512,
+	RS256,
+	RS384,
+	RS512,
+	ES256,
+	ES384,
+	ES512,
+	PS256,
+	PS384,
+	PS512;
+
+}
