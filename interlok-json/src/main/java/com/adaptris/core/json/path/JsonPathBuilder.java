90c4393d7bb3a1f678e1f1b4360250b39ee47a0a
James Wickham <james.wickham@proagrica.com>
2023-09-08 11:40:11 +0100

    adding a json path builder and corresponding test class. Part of ticket: https://adaptris.atlassian.net/browse/INTERLOK-4123

---
+++ interlok-json/src/main/java/com/adaptris/core/json/path/JsonPathBuilder.java
@@ -0,0 +1,110 @@
+package com.adaptris.core.json.path;
+
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.constraints.NotNull;
+
+import com.adaptris.annotation.AdapterComponent;
+import com.adaptris.annotation.AutoPopulated;
+import com.adaptris.annotation.ComponentProfile;
+import com.adaptris.annotation.DisplayOrder;
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.CoreException;
+import com.adaptris.core.ServiceException;
+import com.adaptris.core.security.PathBuilder;
+import com.adaptris.core.util.Args;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamImplicit;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.InvalidPathException;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.PathNotFoundException;
+import com.jayway.jsonpath.ReadContext;
+import com.jayway.jsonpath.spi.json.JsonSmartJsonProvider;
+import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
+
+/**
+ * 
+ * @author jwickham
+ * 
+ *         Imp that expects a JSON path to be provided.
+ *
+ */
+
+@XStreamAlias("json-path-builder")
+@AdapterComponent
+@ComponentProfile(summary = "json builder to extract and insert", tag = "service,security,path", since = "4.8.9")
+@DisplayOrder(order = { "json-paths" })
+public class JsonPathBuilder implements PathBuilder {
+
+  private static final String JSON_PATH_NOT_FOUND_EXCEPTION_MESSAGE = "No results found for JSON path [%s]";
+  private static final String JSON_INVALID_PATH_EXCEPTION_MESSAGE = "Invalid Json path [%s]";
+  private static final String JSON_NON_OBJECT_PATH_EXCEPTION_MESSAGE = "Please ensure your path [%s] points to a single JSON object";
+  
+
+  public JsonPathBuilder() {
+    this.setPaths(new ArrayList<String>());
+  }
+
+  @Getter
+  @Setter
+  @NotNull
+  @AutoPopulated
+  @XStreamImplicit(itemFieldName = "json-paths")
+  private List<String> paths;
+
+  private Configuration jsonConfig = new Configuration.ConfigurationBuilder().jsonProvider(new JsonSmartJsonProvider())
+      .mappingProvider(new JacksonMappingProvider()).options(EnumSet.noneOf(Option.class)).build();
+
+  @Override
+  public void prepare() throws CoreException {
+    Args.notNull(getPaths(), "json-paths");
+  }
+  
+  @Override
+  public Map<String, String> extract(AdaptrisMessage msg) throws ServiceException {
+    String jsonString = msg.getContent();
+    ReadContext context = JsonPath.parse(jsonString, jsonConfig);
+    Map<String, String> pathKeyValuePairs = new LinkedHashMap<>();
+    for (String jsonPath : this.getPaths()) {
+      try {
+        Object result = context.read(jsonPath);
+        if (Map.class.isAssignableFrom(result.getClass()) || List.class.isAssignableFrom(result.getClass())) {
+          System.out.println(result.getClass());
+          throw new ServiceException(String.format(JSON_NON_OBJECT_PATH_EXCEPTION_MESSAGE, jsonPath));
+        }
+        System.out.println(result.getClass());
+        pathKeyValuePairs.put(jsonPath, result.toString());
+      } catch (PathNotFoundException e) {
+        throw new ServiceException(String.format(JSON_PATH_NOT_FOUND_EXCEPTION_MESSAGE, jsonPath));
+      } catch (InvalidPathException e) {
+        throw new ServiceException(String.format(JSON_INVALID_PATH_EXCEPTION_MESSAGE, jsonPath));
+      }
+    }
+    return pathKeyValuePairs;
+  }
+
+  @Override
+  public void insert(AdaptrisMessage msg, Map<String, String> pathKeyValuePairs) throws ServiceException {
+    String jsonString = msg.getContent();
+    DocumentContext jsonDoc = JsonPath.using(jsonConfig).parse(jsonString);
+    for (Map.Entry<String, String> entry : pathKeyValuePairs.entrySet()) {
+      String jsonKey = entry.getKey();
+      DocumentContext jsonValue = JsonPath.using(jsonConfig).parse(entry.getValue());
+      jsonDoc.set(jsonKey, jsonValue.json());
+    }
+    msg.setContent(jsonDoc.jsonString(), msg.getContentEncoding());
+  }
+
+}
---
+++ interlok-json/src/test/java/com/adaptris/core/json/path/JsonPathBuilderTest.java
@@ -0,0 +1,161 @@
+package com.adaptris.core.json.path;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.AdaptrisMessageFactory;
+import com.adaptris.core.ServiceException;
+
+public class JsonPathBuilderTest {
+
+  private static final String JSON = "{\"firstName\":\"John\",\"lastName\":\"doe"
+      + "\",\"age\":26,\"address\":{\"streetAddress\":\"street\",\"city\":\"city\",\"postalCode"
+      + "\":\"TW12 3RR\"},\"phoneNumbers\":[{\"type\":\"mobile\",\"number\":\"000-000-000\"},"
+      + "{\"type\":\"home\",\"number\":\"111-111-111\"}]}";
+  
+  private static final String NON_JSON = "not json";
+
+  private static final String JSON_FIRSTNAME_PATH = "$.firstName";
+  private static final String JSON_LASTNAME_PATH = "$.lastName";
+  private static final String JSON_AGE_PATH = "$.age";
+  private static final String JSON_MULTI_OBJECT_PATH = "$.address";
+  private static final String JSON_ARRAY_PATH = "$.phoneNumbers";
+  private static final String JSON_EXPRESSION_PATH = "$.phoneNumbers.[0].number";
+  private static final String JSON_INVALID_PATH = "invalid json path";
+  private static final String JSON_NON_EXISTENT_PATH = "$.thirdName";
+
+  private static final String JSON_PATH_NOT_FOUND_EXCEPTION_MESSAGE = "No results found for JSON path [%s]";
+  private static final String JSON_INVALID_PATH_EXCEPTION_MESSAGE = "Invalid Json path [%s]";
+  private static final String JSON_NON_OBJECT_PATH_EXCEPTION_MESSAGE = "Please ensure your path [%s] points to a single JSON object";
+  
+  private static Map<String, String> resultKeyValuePairs;
+  private static List<String> jsonPath;
+  private static JsonPathBuilder jsonPathProvider;
+  
+  @BeforeAll
+  public static void setUp() {
+    resultKeyValuePairs  = new LinkedHashMap<>();
+    jsonPath = new ArrayList<String>();
+    jsonPathProvider = new JsonPathBuilder();
+  }
+  
+  @AfterEach
+  public void tearDown() {
+    resultKeyValuePairs.clear();
+    jsonPath.clear();
+  }
+
+  @Test
+  public void testSingleObjectJsonPath() {
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(JSON);
+    jsonPath.add(JSON_FIRSTNAME_PATH);
+    jsonPathProvider.setPaths(jsonPath);
+    try {
+      resultKeyValuePairs = jsonPathProvider.extract(msg);
+      jsonPathProvider.insert(msg, resultKeyValuePairs);
+    } catch (ServiceException e) {
+      e.printStackTrace();
+      fail();
+    }
+    assertEquals(JSON, msg.getContent());
+  }
+  
+  @Test
+  public void testExpressionJsonPath() {
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(JSON);
+    jsonPath.add(JSON_EXPRESSION_PATH);
+    jsonPathProvider.setPaths(jsonPath);
+    try {
+      resultKeyValuePairs = jsonPathProvider.extract(msg);
+      jsonPathProvider.insert(msg, resultKeyValuePairs);
+    } catch (ServiceException e) {
+      e.printStackTrace();
+      fail();
+    }
+    assertEquals(JSON, msg.getContent());
+  }
+  
+  @Test
+  public void testMultipleJsonPaths() {
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(JSON);
+    jsonPath.add(JSON_FIRSTNAME_PATH);
+    jsonPath.add(JSON_LASTNAME_PATH);
+    jsonPath.add(JSON_AGE_PATH);
+    jsonPathProvider.setPaths(jsonPath);
+    try {
+      resultKeyValuePairs = jsonPathProvider.extract(msg);
+      jsonPathProvider.insert(msg, resultKeyValuePairs);
+    } catch (ServiceException e) {
+      e.printStackTrace();
+      fail();
+    }
+    assertEquals(JSON, msg.getContent());
+  }
+  
+  @Test
+  public void testMultiObjectJsonPathException() {
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(JSON);
+    jsonPath.add(JSON_MULTI_OBJECT_PATH);
+    jsonPathProvider.setPaths(jsonPath);
+    Throwable exception =  Assertions.assertThrows(ServiceException.class, () -> {
+      resultKeyValuePairs = jsonPathProvider.extract(msg);
+    });
+    assertEquals(String.format(JSON_NON_OBJECT_PATH_EXCEPTION_MESSAGE, JSON_MULTI_OBJECT_PATH), exception.getMessage());
+  }
+  
+  @Test
+  public void testArrayJsonPathException() {
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(JSON);
+    jsonPath.add(JSON_ARRAY_PATH);
+    jsonPathProvider.setPaths(jsonPath);
+    Throwable exception =  Assertions.assertThrows(ServiceException.class, () -> {
+      resultKeyValuePairs = jsonPathProvider.extract(msg);
+    });
+    assertEquals(String.format(JSON_NON_OBJECT_PATH_EXCEPTION_MESSAGE, JSON_ARRAY_PATH), exception.getMessage());
+  }
+  
+  @Test
+  public void testNonJson() {
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(NON_JSON);
+    jsonPath.add(JSON_FIRSTNAME_PATH);
+    jsonPathProvider.setPaths(jsonPath);
+    Throwable exception =  Assertions.assertThrows(ServiceException.class, () -> {
+      resultKeyValuePairs = jsonPathProvider.extract(msg);
+    });
+    assertEquals(String.format(JSON_PATH_NOT_FOUND_EXCEPTION_MESSAGE, JSON_FIRSTNAME_PATH), exception.getMessage());
+  }
+  
+  @Test
+  public void testNonExistentJsonPath() {
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(JSON);
+    jsonPath.add(JSON_NON_EXISTENT_PATH);
+    jsonPathProvider.setPaths(jsonPath);
+    Throwable exception =  Assertions.assertThrows(ServiceException.class, () -> {
+      resultKeyValuePairs = jsonPathProvider.extract(msg);
+    });
+    assertEquals(String.format(JSON_PATH_NOT_FOUND_EXCEPTION_MESSAGE, JSON_NON_EXISTENT_PATH), exception.getMessage());
+  }
+  
+  @Test
+  public void testInvalidJsonPath() {
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(JSON);
+    jsonPath.add(JSON_INVALID_PATH);
+    jsonPathProvider.setPaths(jsonPath);
+    Throwable exception =  Assertions.assertThrows(ServiceException.class, () -> {
+      resultKeyValuePairs = jsonPathProvider.extract(msg);
+    });
+    assertEquals(String.format(JSON_INVALID_PATH_EXCEPTION_MESSAGE, JSON_INVALID_PATH), exception.getMessage());
+  }
+  
+}
