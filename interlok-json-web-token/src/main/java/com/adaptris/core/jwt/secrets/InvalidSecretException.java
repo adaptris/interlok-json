5f6847c55ffdd609c9a8ef89a18d9033142d0efe
SB <sebastien-belin-adp@users.noreply.github.com>
2023-10-24 17:32:34 +0900

    V5 - INTERLOK-4196 Bump jwtVersion to 0.12.3 (#493)
    * INTERLOK-4196 Bump jwtVersion to 0.12.2
    
    Fix some test that were not working.
    
    * INTERLOK-4196 Bump jwtVersion to 0.12.3
    
    Fix tests
    
    ---------
    
    Co-authored-by: Sebastien Belin <sebastien.belin@proagrica.com>

--- interlok-json-web-token/src/main/java/com/adaptris/core/jwt/secrets/InvalidSecretException.java
+++ interlok-json-web-token/src/main/java/com/adaptris/core/jwt/secrets/InvalidSecretException.java
@@ -1,9 +1,11 @@
 package com.adaptris.core.jwt.secrets;
 
-public class InvalidSecretException extends Exception
-{
-  InvalidSecretException(Exception e)
-  {
+public class InvalidSecretException extends Exception {
+
+  private static final long serialVersionUID = -8523235704649595993L;
+
+  InvalidSecretException(Exception e) {
     super(e);
   }
+
 }
